a8078b3e566831a05e76f106f1ac6154a9b60d27
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-05-20 21:40:10 +0900

    error
    engine.delay error

--- python/test_input.py
+++ python/test_input.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
 import speech_recognition as sr #sst 모듈
 import pyttsx3 #tts 모듈
+import time
 
 engine = pyttsx3.init() #tts모듈 실행
 voices = engine.getProperty('voices')
@@ -28,48 +28,13 @@ def voice_input():
 
             elif '식단' or '메뉴' in text:
                 return "식단"
-            
-            
+                        
             elif '건물' or '위치' in text:
                 return "건물"
         
         except:
             print("Sorry could not recognize what you said\n\n")
 
+a = voice_input()
 
-app = Flask("CBNU_for_blind")
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/index.html")
-def index():
-    word = voice_input()
-    url = ""
-    if word is "날씨":
-        url = "weather.html"
-
-    return render_template("index.html", word = word, url = url)
-
-@app.route("/menu.html")
-def menu():
-    return render_template("menu.html")
-
-@app.route("/navigation.html")
-def navigation():
-    return render_template("navigation.html")
-
-@app.route("/profile.html")
-def profile():
-    return render_template("profile.html")
-
-@app.route("/schedule.html")
-def schedule():
-    return render_template("schedule.html")
-
-@app.route("/weather.html")
-def weather():
-    return render_template("weather.html")
-
-app.run(host="0.0.0.0")
+print(a)
